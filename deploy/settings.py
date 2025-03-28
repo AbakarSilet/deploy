6620c45f1bca81966994076d85c738cd070875f6
AbakarSilet <abakarix4@gmail.com>
2025-03-28 17:16:19 +0100

    Quick update

--- deploy/settings.py
+++ deploy/settings.py
@@ -14,6 +14,11 @@ import os
 from pathlib import Path
 import dj_database_url
 from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+
+# Charger le .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=v)^c@y1%v^eudiymc+f5%uy0@-=5%%j7m_jgqanzy^8dz8s0d'
+SECRET_KEY = SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
