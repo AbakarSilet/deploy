3b7a2565c809de29f2aab4ad6c2e6bb9d0e762a7
AbakarSilet <abakarix4@gmail.com>
2025-03-28 17:37:14 +0100

    Quick update

--- deploy/settings.py
+++ deploy/settings.py
@@ -99,9 +99,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'deploy.wsgi.application'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -165,12 +163,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'  # Dossier pour collectstatic
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',  # Pour les fichiers statiques supplémentaires
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
